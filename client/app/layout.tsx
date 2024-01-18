f7cd189d43616eb4adfa228adaf40d40df24a5a3
Shobhnik13 <shobhnikw@gmail.com>
2024-01-19 04:32:30 +0530

    enabled analytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ 
   subsets: ['latin'] ,
   variable: '--font-inter'})
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <SpeedInsights />
         <Analytics />
         </body>
     </html>
