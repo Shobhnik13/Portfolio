1bbe45876a2a9ca6b23b0ee8cc5bf43e045c7d9f
Shobhnik13 <shobhnikw@gmail.com>
2024-01-29 20:07:20 +0530

    changed resume

--- client/components/HireMe.tsx
+++ client/components/HireMe.tsx
@@ -13,7 +13,7 @@ const HireMe = () => {
             <Button className="hover:scale-95 ease-in-out transition-all duration-100">Schedule a meet / cal.com</Button>
             </Link>
 
-            <Link target="_blank" className="flex text-sm 2xl:text-base hover:underline flex-row gap-x-1 items-center justify-center" href={'https://drive.google.com/file/d/1-g3eK0N73DkjA7ZaY1eUl5fv7GJOucXc/view?usp=sharing'}>Resume <GoArrowUpRight className="text-base mt-1"/> </Link>
+            <Link target="_blank" className="flex text-sm 2xl:text-base hover:underline flex-row gap-x-1 items-center justify-center" href={'https://drive.google.com/file/d/1og90pH5lcd82y4js3p5wsfjh__C9txCG/view?usp=sharing'}>Resume <GoArrowUpRight className="text-base mt-1"/> </Link>
         </div>
     </div>
   )
